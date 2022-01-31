d7deb96bf6b3dd9dad11dddb894229bbf0eed154
Rob Banning <bannjunk@gmail.com>
2022-01-31 07:33:31 -0800

    create and apply initial db migrations

---
+++ BanningApplications.WebApi/Migrations/HookDb/20220131005722_Hook_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BanningApplications.WebApi.Migrations.HookDb
+{
+    public partial class Hook_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HookRequests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 100, nullable: false),
+                    Path = table.Column<string>(nullable: true),
+                    Headers = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HookRequests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HookRequests");
+        }
+    }
+}
---
+++ BanningApplications.WebApi/Migrations/SettingsDb/20220131005647_Settings_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BanningApplications.WebApi.Migrations.SettingsDb
+{
+    public partial class Settings_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(maxLength: 36, nullable: false),
+                    Scope = table.Column<string>(maxLength: 36, nullable: false),
+                    Settings = table.Column<string>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    ModifyDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => new { x.UserId, x.Scope });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+        }
+    }
+}
